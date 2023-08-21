c5a68ff6737d43afa1497a70e1c3722277f84056
maximoortelli <maximoortelli@hotmail.com>
2023-08-21 19:12:28 -0300

    Update files
    Update index.js
    Update section.js

--- index.js
+++ index.js
@@ -26,31 +26,31 @@ class TheLib {
   }
 
   showMyBooks() {
-    const $differentbooks = document.querySelector('.showLib');
-    $differentbooks.innerHTML = '';
+    const differentbooks = document.querySelector('.showLib');
+    differentbooks.innerHTML = '';
 
     this.manybooks.forEach((thebook, i) => {
-      const $multipleBooks = document.createElement('div');
-      const $title = document.createElement('h6');
-      const $X = document.createElement('h6');
-      const $author = document.createElement('h6');
-      const $erase = document.createElement('button');
-      $multipleBooks.classList.add('multiplybooks');
-      $title.classList.add('myTitle');
-      $X.classList.add('x');
-      $author.classList.add('myAut');
-      $erase.classList.add('erase');
-      $title.innerText = `"${thebook.title}"`;
-      $author.textContent = thebook.author;
-      $X.textContent = 'by';
-      $erase.innerHTML = 'Remove';
-      $multipleBooks.appendChild($title);
-      $multipleBooks.appendChild($X);
-      $multipleBooks.appendChild($author);
-      $multipleBooks.appendChild($erase);
-      $erase.addEventListener('click', () => this.erase(i));
+      const multipleBooks = document.createElement('div');
+      const title = document.createElement('h6');
+      const X = document.createElement('h6');
+      const author = document.createElement('h6');
+      const erase = document.createElement('button');
+      multipleBooks.classList.add('multiplybooks');
+      title.classList.add('myTitle');
+      X.classList.add('x');
+      author.classList.add('myAut');
+      erase.classList.add('erase');
+      title.innerText = `"${thebook.title}"`;
+      author.textContent = thebook.author;
+      X.textContent = 'by';
+      erase.innerHTML = 'Remove';
+      multipleBooks.appendChild(title);
+      multipleBooks.appendChild(X);
+      multipleBooks.appendChild(author);
+      multipleBooks.appendChild(erase);
+      erase.addEventListener('click', () => this.erase(i));
 
-      $differentbooks.appendChild($multipleBooks);
+      differentbooks.appendChild(multipleBooks);
     });
   }
 }
@@ -59,25 +59,25 @@ document.addEventListener('DOMContentLoaded', () => {
   const myLibrary = new TheLib();
 
   function totalEmpty() {
-    const $myform = document.querySelector('.Formulary');
-    $myform.reset();
+    const myform = document.querySelector('.Formulary');
+    myform.reset();
   }
 
   function plusTheLib() {
-    const $recTitle = document.querySelector('#title');
-    const $recAuthor = document.querySelector('#autor');
-    myLibrary.plus($recTitle.value, $recAuthor.value);
+    const recTitle = document.querySelector('#title');
+    const recAuthor = document.querySelector('#autor');
+    myLibrary.plus(recTitle.value, recAuthor.value);
     totalEmpty();
   }
 
-  const $myform = document.querySelector('.Formulary');
-  $myform.addEventListener('submit', (e) => {
+  const myform = document.querySelector('.Formulary');
+  myform.addEventListener('submit', (e) => {
     e.preventDefault();
     plusTheLib();
   });
 
-  const $differentbooks = document.querySelector('.showLib');
-  $differentbooks.addEventListener('click', (event) => {
+  const differentbooks = document.querySelector('.showLib');
+  differentbooks.addEventListener('click', (event) => {
     if (event.target.classList.contains('erase')) {
       const mybookU = Array
         .from(event.target.parentNode.parentNode.children).iOf(event.target.parentNode);
